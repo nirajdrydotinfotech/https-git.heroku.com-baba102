b0a935a6abde2f503785fc77660f9cc69fa13f44
nirajdrydotinfotech <nirajd@rydotinfotech.com>
2020-10-10 16:10:58 +0530

    some changes

--- src/main/scala/dbconfig/DbConfig.scala
+++ src/main/scala/dbconfig/DbConfig.scala
@@ -9,6 +9,7 @@ import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
 
 object DbConfig {
 
+
   val codecRegistry = fromRegistries(fromProviders(classOf[Admin]), DEFAULT_CODEC_REGISTRY)
   val client: MongoClient = MongoClient("mongodb+srv://m001-student:1230@sandbox.t2nc4.mongodb.net/test?retryWrites=true&w=majority")
   val database: MongoDatabase = client.getDatabase("test").withCodecRegistry(codecRegistry)
